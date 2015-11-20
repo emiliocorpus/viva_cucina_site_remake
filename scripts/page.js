b8cd519687ba61b5345d5f3f701c80f3a3a9a9b7
Emilio Corpus <emiliocorpus@gmail.com>
2015-11-19 19:09:04 -0800

    adds previous web files

---
+++ scripts/page.js
@@ -0,0 +1,76 @@
+//an image width in pixels 
+var imageWidth = 600;
+var currentImage = 0;
+//set image count 
+var allImages = 6;
+var slideshow;
+
+//DOM and all content is loaded 
+$(window).ready(function() {
+
+	//start slideshow
+	slideshow=setInterval(startAutoSlideshow, 4000)
+
+	//setup slideshow frame width
+	$('#slideshow ul').width(allImages*imageWidth);
+
+	//attach click event to slideshow buttons
+	$('.slideshow-next').click(function(){
+		event.preventDefault()
+		//increase image counter
+		currentImage++;
+		//if we are at the end let set it to 0
+		if(currentImage>=allImages) currentImage = 0;
+		//calcualte and set position
+		setFramePosition(currentImage);
+
+	});
+
+	$('.slideshow-prev').click(function(){
+		event.preventDefault()
+		//decrease image counter
+		currentImage--;
+		//if we are at the end let set it to 0
+		if(currentImage<0) currentImage = allImages-1;
+		//calcualte and set position
+		setFramePosition(currentImage);
+
+	});
+
+	$('#slideshow').mouseenter(function(){
+		console.log("clearing..")
+		clearInterval(slideshow)
+	})
+
+	$('#slideshow').mouseleave(function(){
+		console.log('starting')
+		slideshow= setInterval(startAutoSlideshow,4000)
+	})
+
+});
+
+function startAutoSlideshow() {
+	if (currentImage < 5) {
+		currentImage++
+		setFramePosition(currentImage)
+	}
+	else if (currentImage == 5) {
+		currentImage = 0
+		setFramePosition(currentImage)
+	}
+}
+
+//calculate the slideshow frame position and animate it to the new position
+function setFramePosition(pos){
+
+	//calculate position
+		var px = imageWidth*pos*-1;
+		//set ul left position
+		$('#slideshow ul').animate({
+		opacity: 0.9,
+		left: px
+		}, 1200);
+		$('#slideshow ul').animate({
+		opacity:1.0,
+		}, 200)
+}
